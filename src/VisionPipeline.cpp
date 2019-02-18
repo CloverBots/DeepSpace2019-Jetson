d5732a17a25b218daac33472c0801ee26c7c9005
isaiahwalton <console.logisaiah@gmail.com>
2019-02-18 15:15:11 -0800

    made it work
    nuf sed

--- src/VisionPipeline.cpp
+++ src/VisionPipeline.cpp
@@ -6,27 +6,34 @@ VisionPipeline::VisionPipeline(){};
 std::vector<std::vector<cv::Point> > VisionPipeline::Process(cv::Mat& source0)
 {
 	cv::Mat input = source0;
-	double hslThresholdHue[] = { 0.0, 36.0 };
-	double hslThresholdSaturation[] = { 0.0, 57.0 };
-	double hslThresholdLuminance[] = { 248.0, 255.0 };
+	double hslThresholdHue[] = { 0.0, 40.0 };
+	double hslThresholdSaturation[] = { 0.0, 70.0 };
+	double hslThresholdLuminance[] = { 250.0, 255.0 };
 	cv::cvtColor(input, input, cv::COLOR_BGR2HLS);
+	//cv::imshow("idk", input);
+	//cv::waitKey();
 	cv::inRange(input, cv::Scalar(hslThresholdHue[0], hslThresholdLuminance[0], hslThresholdSaturation[0]), 
 					 cv::Scalar(hslThresholdHue[1], hslThresholdLuminance[1], hslThresholdSaturation[1]), input);
+	//cv::imshow("idk", input);
+	//cv::waitKey();
+
 	//cv::cvtColor(input, input, cv::COLOR_HLS2BGR);
 	//imshow("a", input);
 	//cv::waitKey(0);	
 	cv::Point cvErodeAnchor(-1, -1);
-	double cvErodeIterations = 3.0;
+	double cvErodeIterations = 0.0;
 	cv::Mat cvErodeKernel;
  	int cvErodeBordertype = cv::BORDER_CONSTANT;
 	cv::Scalar cvErodeBordervalue(-1);
 	cv::erode(input, input, cvErodeKernel, cvErodeAnchor, (int)cvErodeIterations, cvErodeBordertype, cvErodeBordervalue);
 	cv::Mat cvDilateKernel;
 	cv::Point cvDilateAnchor(-1, -1);
-	double cvDilateIterations = 4.0;  // default Double
+	double cvDilateIterations = 5.0;  // default Double
     	int cvDilateBordertype = cv::BORDER_CONSTANT;
 	cv::Scalar cvDilateBordervalue(-1);
 	cv::dilate(input, input, cvDilateKernel, cvDilateAnchor, (int)cvDilateIterations, cvDilateBordertype, cvDilateBordervalue);
+	//	cv::imshow("idk", input);
+	//cv::waitKey();
 	std::vector<std::vector<cv::Point> > contours;
 	bool findContoursExternalOnly = false;
 	std::vector<cv::Vec4i> hierarchy;
@@ -34,13 +41,13 @@ std::vector<std::vector<cv::Point> > VisionPipeline::Process(cv::Mat& source0)
 	int mode = findContoursExternalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
 	int method = cv::CHAIN_APPROX_SIMPLE;
 	cv::findContours(input, contours, hierarchy, mode, method);
-	double filterContoursMinArea = 700.0;
+	double filterContoursMinArea = 10.0;
 	double filterContoursMinPerimeter = 0; 
 	double filterContoursMinWidth = 0;
 	double filterContoursMaxWidth = 1000;
 	double filterContoursMinHeight = 0;
 	double filterContoursMaxHeight = 1000;
-	double filterContoursSolidity[] = {0, 100};
+	double filterContoursSolidity[] = {0, 1000};
 	double filterContoursMaxVertices = 1000000;
 	double filterContoursMinVertices = 0;
 	double filterContoursMinRatio = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,14 @@
 #include "VisionPipeline.h"
 #include "networktables/NetworkTable.h"
 #include <chrono>
+#include <fstream>
 
 typedef std::chrono::high_resolution_clock Clock;
 
 #define OUTPUT_VALUES true
+
+const int angle_min = 3;
+const int angle_max = 40;
 
 int device = 0;
 int width = 1280;
@@ -17,30 +21,79 @@ int bitrate = 600000;
 int port = 5805;
 std::string videoSinkIP = "10.36.74.2";
 
+int closest(std::vector<cv::Point2f> vec, int value)
+{
+	std::vector<int> distances;
+	int Closest = value;
+	int val = value;
+	for(int i = 0; i < vec.size(); i++)
+	{	
+		distances.push_back(abs(value - vec[i].x));
+		std::cout << abs(value - vec[i].x) << "  " << i << std::endl;
+	}
+	if(vec.size() != 0)
+	{	
+		Closest = 10000;
+	}
+	for(int i = 0; i < distances.size(); i++)
+	{
+		if(distances.at(i) < Closest)
+		{
+			//std::cout << "closest  "<< distances.at(i) <<"  " << i << std::endl;  
+			Closest = distances.at(i);
+			val = i;
+		}
+	}
+	
+	//std::cout << "closest val "<< distances.at(val) << std::endl;
+	
+	if(vec.size() != 0)
+	{	
+		return vec[val].x;
+	}
+	else
+	{
+		return 1280/2;
+	}
+}
+
 int main()
 {
+	std::ofstream file {"file.txt"};
 	CvCapture_GStreamer mycam;
+	std::cout << "starting pipeline" << std::endl;
     	string read_pipeline = createReadPipelineSplit (
             device, width, height, framerate, mjpeg, 
             bitrate, videoSinkIP, port);
-
-	mycam.open (CV_CAP_GSTREAMER_FILE, read_pipeline.c_str());
+	while(true)
+	{
+		std::cout << "restarting pipeline" << std::endl;
+		if(mycam.open (CV_CAP_GSTREAMER_FILE, read_pipeline.c_str()))
+		{
+			break;
+		}
+	}
 	printf ("Succesfully opened camera with dimensions: %dx%d\n",
             width, height);
 
 	VisionPipeline pipeline;
-	
-	//NetworkTable::SetClientMode();
-	//NetworkTable::SetTeam(3674);
-	//NetworkTable::SetIPAddress("10.36.74.61");
-	//table = NetworkTable::GetTable("vision");
+	std::shared_ptr<NetworkTable> table;
+	NetworkTable::SetClientMode();
+	NetworkTable::SetTeam(3674);
+	NetworkTable::SetIPAddress("10.36.74.2");
+	table = NetworkTable::GetTable("vision");
 
 	
 	std::vector<std::vector<cv::Point> > contours;
 	std::vector<cv::Point2f> contour_center;
+	std::vector<cv::Point2f> data_center;
+	std:vector<int> data_size;
 	std::vector<float> contour_angle;
-
+	int lost_frames;
+	int prev_data;
 	cv::Mat frame;
+	int tempx;
+	int tempy;
 	//cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 	//std::cout << contours.size() << std::endl;
 	for (;;)
@@ -50,7 +103,10 @@ int main()
 		IplImage* img = mycam.retrieveFrame(0);
 
 		frame = cv::cvarrToMat(img).clone();
+		//cv::imshow("idk", frame);
+		//cv::waitKey();
 		contours = pipeline.Process(frame);
+		//std::cout << contours.size() << std::endl;
 		for (int i = 0; i < contours.size(); ++i)
 		{
 			cv::RotatedRect rotatedRect = cv::minAreaRect(contours[i]);
@@ -79,9 +135,9 @@ int main()
 			}
 			angle = -angle;
 			contour_angle.push_back(angle);
-
+			//std::cout << angle << std::endl;
 			for (unsigned int j = 0; j < 4; ++j)
-				cv::line(frame, rect_points[j], rect_points[(j + 1) % 4], cv::Scalar(0, 255, 0), 2);
+				cv::line(frame, rect_points[j], rect_points[(j + 1) % 4], cv::Scalar(0, 0, 255), 2);
 
 			std::stringstream ss;
 			ss << angle << ", " << i;
@@ -108,13 +164,15 @@ int main()
 					{
 						skip = false;
 					}
-				}
+				}		
+				//std::cout << u << "   " << v << std::endl;
 				if (!skip)
 				{
 					if (contour_center.at(u).x != contour_center.at(v).x && contour_center.at(u).y != contour_center.at(v).y)
 					{
-						if ((contour_center.at(u).y < contour_center.at(v).y + 30) && (contour_center.at(u).y > contour_center.at(v).y - 30))
-						{
+						//std::cout << "contour not self" << std::endl;
+						//if ((contour_center.at(u).y < contour_center.at(v).y + 30) && (contour_center.at(u).y > contour_center.at(v).y - 30))
+						//{
 							if (contour_center.at(u).x < contour_center.at(v).x)
 							{
 								//std::cout << "swap: " << u << " " << v << std::endl;
@@ -124,44 +182,100 @@ int main()
 
 							for (int i = 0; i < contour_center.size(); i++)
 							{
-								//std::cout << " u " << u << "  " << contour_center.at(u).x << " i " << i << "  " << contour_center.at(i).x << " v " << v << "  " << contour_center.at(v).x << std::endl;
-
+								
 								if (contour_center.at(u).x > contour_center.at(i).x && contour_center.at(v).x < contour_center.at(i).x)
 								{
-									skip_ = true;
-									//std::cout << "break" << std::endl;
-									break;
+									//std::cout << i << "   "<< u <<"   "<<v<< std::endl;
+											
+									if ((contour_angle.at(i) > angle_min && contour_angle.at(i) < angle_max) || (contour_angle.at(i) < -angle_min && contour_angle.at(i) > -angle_max))
+									{
+
+										//cv::circle(frame, cv::Point(contour_center.at(i).x, contour_center.at(i).y), 10, cv::Scalar(0, 0, 0), 2);
+										//std::cout << contour_angle.at(i) <<std::endl;
+										skip_ = true;
+										//std::cout << "break" << std::endl;
+										break;
+									}
+									
+									tempx = contour_center.at(i).x;
+									tempy = contour_center.at(i).y;
 								}
 								else
 								{
+									//std::cout << u << "   " << v << std::endl;
+									tempx = 0;
+									tempy = 0;
 									skip_ = false;
 								}
 
 							}
+							//std::cout << tempx << "   " << tempy << std::endl; 
+							//std::cout << "end frame" << std::endl;
 							if (!skip_)
 							{
-								//std::cout << "pos: " << u << "  " << v << " angle: " << contour_angle.at(u) << "  " << contour_angle.at(v) << std::endl;
-								if ((contour_angle.at(v) > -3 && contour_angle.at(v) < 20 && contour_angle.at(u) < 3 && contour_angle.at(u) > -20))
+								if ((contour_angle.at(v) > angle_min && contour_angle.at(v) < angle_max && contour_angle.at(u) < -angle_min && contour_angle.at(u) > -angle_max))
 								{
-									cv::circle(frame, cv::Point((contour_center.at(u).x + contour_center.at(v).x) / 2, (contour_center.at(u).y + contour_center.at(v).y) / 2), 5, cv::Scalar(255, 0, 0), 2);
+									//std::cout<< "contour found" << std::endl;
+									data_center.push_back(cv::Point2f((contour_center.at(u).x + contour_center.at(v).x) / 2,
+										 (contour_center.at(u).y + contour_center.at	(v).y) / 2));
+									data_size.push_back(contour_center.at(u).x - contour_center.at(v).x);
+							cv::circle(frame, cv::Point(tempx,tempy), 5, cv::Scalar(255, 255, 255), 2);
+										
+									cv::circle(frame, cv::Point((contour_center.at(u).x + contour_center.at(v).x) / 2,
+										 (contour_center.at(u).y + contour_center.at	(v).y) / 2), 5, cv::Scalar(255, 0, 0), 2);
 								}
 							}
-						}
+						//}
 					}
 				}
 			}
 			
 		}
+		cv::circle(frame, cv::Point(closest(data_center, 1280/2),720/2), 7, cv::Scalar(255, 255, 255), 4);
+		cv::circle(frame, cv::Point(1280/2,720/2), 7, cv::Scalar(255, 0, 255), 4);
 		auto t2 = Clock::now();
-
-		std::cout << "Delta t2-t1: "
-			<< std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
-			<< " milliseconds" << std::endl;
-		std::cout << contour_center.at(0).x << std::endl;
-
-		cv::imshow("mask", frame);
+		/*if(data_center.size() < 0)
+		{
+			lost_frames++;
+		}
+		if(data_center.size() > 0 && lost_frames > 0)
+		{
+			lost_frames = 0;
+		}
+		if(lost_frames == 0)
+		{
+			table->PutNumber("data_amount", data_center.size());
+			prev_data = data_center.size();
+		}
+		else if(lost_frames > 0)
+		{
+			table->PutNumber("data_amount", data_center.size());
+		}
+		else if(lost_frames > 10)
+		{
+			table->PutNumber("data_amount", 0);
+		}
+		else
+		{}*/
+			
+		//std::cout << "Delta t2-t1: "
+		//	<< std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
+		//	<< " milliseconds" << std::endl;
+		//std::cout << contour_center.at(0).x << std::endl;
+		table->PutNumber("data_amount", data_center.size());
+		
+		for(int i = 0; i < data_center.size(); i++)
+		{
+			//std::cout<<data_center.at(i).y<<std::endl;
+			table->PutNumber("data_center"+i, data_center.at(i).x);
+			table->PutNumber("data_size"+i, data_size.at(i));
+		}
+		//cv::imshow("mask", frame);
+		//cv::waitKey();
 		contour_angle.clear();
 		contour_center.clear();
+		data_size.clear();
+		data_center.clear();
 		contours.clear();
 	}
 	return 0;
